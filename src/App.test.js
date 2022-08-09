afbf9b8d1c90824aa9575d6e690c0e2ae982d37a
Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>
2022-08-09 17:37:16 +0530

    Converting JS to TS.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Contract menu', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Contract/i);
   expect(linkElement).toBeInTheDocument();
 });
