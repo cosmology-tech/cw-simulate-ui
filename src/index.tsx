b3bc54e746121f9a969ab7d05cead3d7c82311fa
Bun <Bunchhieng@gmail.com>
2022-09-09 14:46:34 -0400

    Added react-router-dom (#27)

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from "recoil";
-
 import './styles/styles.scss';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MenuDrawer from "./components/MenuDrawer";
+import Home from "./components/Home";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App/>
+      <BrowserRouter>
+        <MenuDrawer/>
+        <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          <Route
+            path="*"
+            element={
+              <main style={{padding: "1rem"}}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>
 );
@@ -18,4 +31,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
