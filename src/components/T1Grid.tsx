9e4e200fab7290d3214f869032db89361daad74f
Bun <Bunchhieng@gmail.com>
2022-09-13 00:35:24 -0400

    Updated Chains page (#35)
    * Updated Chains page
    
    * updated chains state
    
    * updated

---
+++ src/components/T1Grid.tsx
@@ -0,0 +1,69 @@
+import { Box, Grid, IconButton, Typography } from "@mui/material";
+import React from "react";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+
+interface IProps {
+  items?: any[],
+  rightButton?: React.ReactNode,
+  hasRightDeleteButton?: boolean,
+  handleDeleteItem?: (e: any) => void,
+}
+
+const Item = styled(Paper)(({theme}) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  margin: theme.spacing(1),
+  color: theme.palette.text.secondary,
+}));
+
+const T1Grid = ({items, rightButton, hasRightDeleteButton, handleDeleteItem}: IProps) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        typography: "body1",
+        marginTop: 4,
+        marginBottom: 4,
+        display: "flex",
+        justifyContent: "center",
+        flexGrow: 1, padding: "2rem"
+      }}
+    >
+      <Grid spacing={2} sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        placeItems: "center",
+        padding: "10px",
+      }} container xs={11} md={11} lg={10}>
+        {items?.map(el => el.trim()).map((item) => {
+          return (
+            <>
+              <Grid item xs={11} md={11} lg={10}>
+                <Item>
+                  <div style={{display: 'flex'}}>
+                    <Link to={item} style={{flexGrow: 1, textDecoration: 'none', color: "unset"}}>
+                      <Typography variant="h6">{item}</Typography>
+                    </Link>
+                    {rightButton}
+                    {hasRightDeleteButton && (
+                      <IconButton aria-label="delete">
+                        <DeleteForeverIcon/>
+                      </IconButton>
+                    )}
+                  </div>
+                </Item>
+              </Grid>
+            </>
+          );
+        })}
+      </Grid>
+    </Box>
+  )
+}
+
+export default T1Grid;
--- src/components/chains/Chains.tsx
+++ src/components/chains/Chains.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
@@ -6,22 +7,20 @@ import {
   DialogContentText,
   DialogTitle,
   Grid,
+  Stack,
   TextField,
   Typography
 } from "@mui/material";
 import { useRecoilState } from "recoil";
-import { chainsState } from "../../atoms/chainsState";
-import { GREY_3 } from "../../configs/variables";
 import React, { useState } from "react";
 import { snackbarNotificationState } from "../../atoms/snackbarNotificationState";
-import { ChainConfig } from "../../utils/setupSimulation";
 import { Outlet, useParams } from "react-router-dom";
+import { ScreenSearchDesktopOutlined } from "@mui/icons-material";
+import T1Grid from "../T1Grid";
 
 const Chains = () => {
-  const [chains, setChains] = useRecoilState(chainsState);
   const [openDialog, setOpenDialog] = useState(false);
-  const [chainConfig, setChainConfig] = useState<ChainConfig>({} as ChainConfig);
-  const [chainNamesTextField, setChainNamesTextField] = useState<string>("");
+  const [chainNamesTextField, setChainNamesTextField] = useState<string[]>([]);
   const [snackbarNotification, setSnackbarNotification] = useRecoilState(
     snackbarNotificationState
   );
@@ -40,45 +39,40 @@ const Chains = () => {
       ...snackbarNotification,
       severity: "success",
       open: true,
-      message: "Successfully added new chain config.",
+      message: `Successfully added ${chainNamesTextField.length} new chains.`,
     });
     setOpenDialog(false);
   }
 
-  const handleChainConfigChange = (val: string) => {
-    try {
-      const newChainConfig = JSON.parse(val) as ChainConfig;
-      setChainConfig(newChainConfig);
-    } catch (e) {
-      setSnackbarNotification({
-        ...snackbarNotification,
-        severity: "error",
-        open: true,
-        message: "Invalid JSON. Please check your input.",
-      });
-    }
-  }
-
   return (
-    <>
+    <Box
+      sx={{
+        width: "100%",
+        typography: "body1",
+        marginTop: 4,
+        marginBottom: 4,
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       {param.id === undefined ? (<Grid sx={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           placeItems: "center",
           padding: "10px",
-        }} container>
-          <Typography variant="h4" sx={{flexGrow: 1}}>
+        }} container xs={11} md={11} lg={10}>
+          <Typography variant="h5" sx={{flexGrow: 1}}>
             Chains
           </Typography>
           <Button variant="contained" color="primary" onClick={handleClickOpen}>
             Add Chain
           </Button>
           <Dialog open={openDialog} onClose={handleClose}>
-            <DialogTitle>Add New Chain</DialogTitle>
+            <DialogTitle>Add New Chains</DialogTitle>
             <DialogContent>
               <DialogContentText>
-                Enter chain names separated by commas. i.e. "phoenix-1", "osmosis-1", "terra-1".
+                Enter chain names separated by commas. i.e. phoenix-1, osmosis-1, terra-1
               </DialogContentText>
               <TextField
                 autoFocus
@@ -88,7 +82,9 @@ const Chains = () => {
                 type="text"
                 fullWidth
                 variant="standard"
-                onChange={(e) => setChainNamesTextField(e.target.value)}
+                onChange={(e) => {
+                  setChainNamesTextField([...chainNamesTextField, ...e.target.value.split(",").map(e => e.trim())]);
+                }}
               />
             </DialogContent>
             <DialogActions>
@@ -96,27 +92,28 @@ const Chains = () => {
               <Button onClick={handleAddChain}>Add</Button>
             </DialogActions>
           </Dialog>
-          {chains.length > 0 ? (chains.map((chain) => (
-              <Grid item xs={12} sx={{marginTop: 4, marginBottom: 4, width: "60%"}}>
-                <p>test</p>
-              </Grid>))) :
-            (<Grid item xs={12} sx={{
+          {!openDialog && chainNamesTextField.length > 0 ?
+            (<T1Grid items={[...new Set(chainNamesTextField)]} hasRightDeleteButton={true}/>)
+            : (<Grid item xs={12} sx={{
               display: 'grid',
               marginTop: 4,
               marginBottom: 4,
               width: "60%",
-              border: `1px solid ${GREY_3}`,
-              borderRadius: "10px",
+              borderRadius: "5px",
               height: "50vh",
               alignItems: "center",
             }}>
-              <Typography variant="h6" sx={{textAlign: "center"}}>
-                No chain configuration found. Please add a chain configuration.
-              </Typography>
+              <Stack sx={{textAlign: 'center', alignItems: 'center'}}>
+                <ScreenSearchDesktopOutlined sx={{fontSize: "100px"}}/>
+                <Typography variant="h6">
+                  No chain found. Please add a chain.
+                </Typography>
+              </Stack>
             </Grid>)}
-        </Grid>) :
-        <Outlet/>}
-    </>
+        </Grid>)
+        : <Outlet/>
+      }
+    </Box>
   );
 }
 
