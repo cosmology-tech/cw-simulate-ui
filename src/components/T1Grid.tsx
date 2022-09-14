3bc18bb93d0540e101d47388e8f4d4216260efbc
Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>
2022-09-14 12:46:02 +0530

    Added Instances page.

--- src/components/T1Grid.tsx
+++ src/components/T1Grid.tsx
@@ -9,22 +9,24 @@ import T1Link from "./T1Link";
 import { snackbarNotificationState } from "../atoms/snackbarNotificationState";
 
 interface IProps {
-  items?: any[],
-  rightButton?: React.ReactNode,
-  hasRightDeleteButton?: boolean,
-  handleDeleteItem?: (e: any) => void,
+  items?: any[];
+  rightButton?: React.ReactNode;
+  hasRightDeleteButton?: boolean;
+  handleDeleteItem?: (e: any) => void;
 }
 
-const Item = styled(Paper)(({theme}) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   margin: theme.spacing(1),
   color: theme.palette.text.secondary,
 }));
 
-const T1Grid = ({items, rightButton, hasRightDeleteButton}: IProps) => {
-  const [chainNamesTextField, setChainNamesTextField] = useRecoilState<string[]>(chainNamesTextFieldState);
+const T1Grid = ({ items, rightButton, hasRightDeleteButton }: IProps) => {
+  const [chainNamesTextField, setChainNamesTextField] = useRecoilState<
+    string[]
+  >(chainNamesTextFieldState);
   const [snackbarNotification, setSnackbarNotification] = useRecoilState(
     snackbarNotificationState
   );
@@ -34,7 +36,9 @@ const T1Grid = ({items, rightButton, hasRightDeleteButton}: IProps) => {
     const item = e.currentTarget.parentElement;
     item.remove();
     // Remove the item from the chainNamesTextFieldState
-    const newChainNames = chainNamesTextField.filter((el) => el !== item.innerText);
+    const newChainNames = chainNamesTextField.filter(
+      (el) => el !== item.innerText
+    );
     setChainNamesTextField(newChainNames);
     setSnackbarNotification({
       ...snackbarNotification,
@@ -42,7 +46,7 @@ const T1Grid = ({items, rightButton, hasRightDeleteButton}: IProps) => {
       message: `Chain ${item.innerText} deleted`,
       severity: "success",
     });
-  }
+  };
   return (
     <Box
       sx={{
@@ -52,28 +56,38 @@ const T1Grid = ({items, rightButton, hasRightDeleteButton}: IProps) => {
         marginBottom: 4,
         display: "flex",
         justifyContent: "center",
-        flexGrow: 1, padding: "2rem"
+        flexGrow: 1,
+        padding: "2rem",
       }}
     >
-      <Grid spacing={2} sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        placeItems: "center",
-      }} container xs={12} md={12} lg={12}>
+      <Grid
+        spacing={2}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          placeItems: "center",
+        }}
+        container
+        xs={12}
+        md={12}
+        lg={12}
+      >
         {items?.map((item) => {
           return (
             <>
               <Grid item xs={12} md={12} lg={12}>
                 <Item key={item + "item"}>
-                  <div style={{display: 'flex'}}>
-                    <T1Link to={item} sx={{flexGrow: 1}}>
-                      <Typography variant="h6" sx={{paddingLeft: 3}}>{item}</Typography>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <T1Link to={item} sx={{ flexGrow: 1 }}>
+                      <Typography variant="h6" sx={{ paddingLeft: 3 }}>
+                        {item}
+                      </Typography>
                     </T1Link>
                     {rightButton}
                     {hasRightDeleteButton && (
                       <IconButton aria-label="delete" onClick={handleDelete}>
-                        <DeleteForeverIcon/>
+                        <DeleteForeverIcon />
                       </IconButton>
                     )}
                   </div>
@@ -84,7 +98,7 @@ const T1Grid = ({items, rightButton, hasRightDeleteButton}: IProps) => {
         })}
       </Grid>
     </Box>
-  )
-}
+  );
+};
 
 export default T1Grid;
--- src/components/chains/Instances.tsx
+++ src/components/chains/Instances.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Button, Grid, Typography } from "@mui/material";
+import T1Grid from "../T1Grid";
 
 const Instances = () => {
-  return <>Instances</>;
+  return (
+    <>
+      <Grid item xs={12} sx={{ display: "flex", justifyContent: "end" }}>
+        <Grid
+          item
+          xs={4}
+          sx={{ display: "flex", justifyContent: "end", mr: 6 }}
+        >
+          <Button variant="contained" sx={{ borderRadius: 2 }}>
+            <Typography variant="button">New Instance</Typography>
+          </Button>
+        </Grid>
+      </Grid>
+      <T1Grid items={["Instance1", "Instance2"]} rightButton={true} />
+    </>
+  );
 };
 
 export default Instances;
