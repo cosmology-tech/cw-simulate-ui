2e616b52f46b7c76cd198f9ae4192ba10d688699
Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>
2022-09-14 15:57:39 +0530

    Added Codes Screen

--- src/components/chains/Codes.tsx
+++ src/components/chains/Codes.tsx
@@ -1,5 +1,32 @@
+import { Button, Grid, Typography } from "@mui/material";
+import React from "react";
+import T1Grid from "../T1Grid";
+
 const Codes = () => {
-  return <p>Codes</p>;
+  return (
+    <>
+      <Grid item xs={12} sx={{ display: "flex", justifyContent: "end" }}>
+        <Grid
+          item
+          xs={4}
+          sx={{ display: "flex", justifyContent: "end", mr: 6 }}
+        >
+          <Button variant="contained" sx={{ borderRadius: 2 }}>
+            <Typography variant="button">Upload Code</Typography>
+          </Button>
+        </Grid>
+      </Grid>
+      <T1Grid
+        items={["Phoneix1", "Phoneix2"]}
+        rightButton={
+          <Button variant="contained" sx={{ borderRadius: 2 }}>
+            <Typography variant="button">Instantiate</Typography>
+          </Button>
+        }
+        hasRightDeleteButton={true}
+      />
+    </>
+  );
 };
 
 export default Codes;
