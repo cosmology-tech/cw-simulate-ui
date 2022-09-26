c9e4b8976043ddb62c9500dcba410bac0870ebfa
dabralyogesh <dabralyogesh@gmail.com>
2022-09-26 17:53:25 +0530

    Added json validator for JSON editors. (#79)
    Co-authored-by: Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>

---
+++ src/atoms/jsonErrorState.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const jsonErrorState = atom({
+  key: 'jsonErrorState',
+  default: '',
+});
--- src/components/JsonCodeMirrorEditor.tsx
+++ src/components/JsonCodeMirrorEditor.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactCodeMirror from "@uiw/react-codemirror";
 import { json } from "@codemirror/lang-json";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { GREY_6 } from "../configs/variables";
+import { validateJSON } from "../utils/fileUtils";
+import { useRecoilState } from "recoil";
+import { jsonErrorState } from "../atoms/jsonErrorState";
 
 interface IJsonCodeMirrorEditorProps {
   jsonValue: string;
@@ -18,24 +21,58 @@ export const JsonCodeMirrorEditor = ({
   const defaultPlaceholder = placeholder || {
     json: "Enter your json here",
   };
+  const [jsonError, setJsonError] = useRecoilState(jsonErrorState);
   return (
     <Grid
       item
       sx={{
-        overflow: "scroll",
-        padding: 2,
-        border: `1px solid ${GREY_6}`,
+        display: "flex",
+        flexDirection: "column",
         height: "100%",
         width: "100%",
       }}
     >
-      <ReactCodeMirror
-        value={jsonValue}
-        extensions={[json()]}
-        onChange={(val: string) => setPayload(val)}
-        placeholder={JSON.stringify(defaultPlaceholder, null, 2)}
-        style={{ border: "none", height: "100%" }}
-      />
+      <Grid
+        item
+        sx={{
+          overflow: "scroll",
+          border: `1px solid ${GREY_6}`,
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <ReactCodeMirror
+          value={jsonValue}
+          extensions={[json()]}
+          onChange={(val: string) => {
+            setPayload(val);
+            if (val.length == 0) {
+              setJsonError("");
+              return;
+            }
+            try {
+              const parsedJSON = JSON.parse(val);
+              if (!validateJSON(parsedJSON, {})) {
+                //TODO: Show correct error message when validate message functionality changes.
+                setJsonError("Invalid JSON");
+              } else {
+                setJsonError("");
+              }
+            } catch {
+              setJsonError("Invalid JSON");
+            }
+          }}
+          placeholder={JSON.stringify(defaultPlaceholder, null, 2)}
+          style={{ border: "none", height: "100%" }}
+        />
+      </Grid>
+      {jsonError && (
+        <Grid item sx={{ mt: 1 }}>
+          <Typography variant="subtitle2" color="red">
+            Invalid JSON
+          </Typography>
+        </Grid>
+      )}
     </Grid>
   );
 };
