2d5f18fd88c1d05759078319b4bec6076e7e71d0
kiruse <26940742+Kiruse@users.noreply.github.com>
2022-09-28 13:52:40 +0200

    add Instances to Sidebar & add proper link to Code item

--- src/components/MenuDrawer/CodeMenuItem.tsx
+++ src/components/MenuDrawer/CodeMenuItem.tsx
@@ -15,6 +15,7 @@ export default function CodeMenuItem(props: ICodeMenuItemProps) {
     <T1MenuItem
       label={code.name}
       nodeId={`${chainId}/codes/${code.name}`}
+      link={`/chains/${chainId}#codes`}
       textEllipsis
     />
   )
---
+++ src/components/MenuDrawer/InstancesMenuItem.tsx
@@ -0,0 +1,30 @@
+import { useRecoilValue } from "recoil";
+import filteredInstancesFromChainId from "../../selectors/filteredInstancesFromChainId";
+import InstanceMenuItem from "./InstanceMenuItem";
+import T1MenuItem from "./T1MenuItem";
+
+export interface IInstancesMenuItemProps {
+  chainId: string;
+}
+
+export default function InstancesMenuItem(props: IInstancesMenuItemProps) {
+  const { chainId } = props;
+  
+  const instances = useRecoilValue(filteredInstancesFromChainId(chainId));
+  
+  return (
+    <T1MenuItem
+      label="Instances"
+      nodeId={`${chainId}/instances`}
+      link={`/chains/${chainId}#codes`}
+    >
+      {instances.map(instance => (
+        <InstanceMenuItem
+          key={instance.contractAddress}
+          chainId={chainId}
+          instance={instance}
+        />
+      ))}
+    </T1MenuItem>
+  )
+}
