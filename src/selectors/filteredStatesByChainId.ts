370ff06eab33dd3ebdba6932be03690defe5c2b4
kiruse <26940742+Kiruse@users.noreply.github.com>
2022-09-30 01:03:54 +0200

    improve states display

--- src/selectors/filteredStatesByChainId.ts
+++ src/selectors/filteredStatesByChainId.ts
@@ -1,18 +1,27 @@
 import { selectorFamily } from "recoil";
 import cwSimulateEnvState from "../atoms/cwSimulateEnvState";
 
+interface States {
+  [contract: string]: {
+    [key: string]: string;
+  };
+}
+
 const filteredStatesByChainId = selectorFamily({
   key: "filteredStatesByChainId",
-  get: (chainId: string) => ({get}) => {
+  get: (chainId: string) => ({get}): States => {
     const simulation = get(cwSimulateEnvState);
     if (!(chainId in simulation.chains)) return {};
     
-    const result: Record<string, any> = {};
+    const states: States = {};
     
     const decoder = new TextDecoder();
     const chain = simulation.chains[chainId];
     for (const address in chain.contracts) {
       const contract = chain.contracts[address];
+      states[address] = {};
+      const currStates = states[address];
+      
       for (const bkey of contract.storage.keys()) {
         const key = decoder.decode(bkey);
         const value = decoder.decode(contract.storage.get(bkey) ?? new Uint8Array(0));
@@ -21,16 +30,16 @@ const filteredStatesByChainId = selectorFamily({
           // TODO: recursive
           for (const prop in json) {
             const subkey = prop.match(/['".\s-]/) ? `${key}["${prop}"]` : `${key}.${prop}`;
-            result[subkey] = json[prop] + '';
+            currStates[subkey] = json[prop] + '';
           }
         }
         catch {
-          result[key] = value;
+          currStates[key] = value;
         }
       }
     }
     
-    return result;
+    return states;
   }
 });
 
