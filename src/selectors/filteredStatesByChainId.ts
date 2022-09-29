b0d040cd7b115b486386581e1e2cee34cc733c15
kiruse <26940742+Kiruse@users.noreply.github.com>
2022-09-30 00:31:58 +0200

    rudimentary impl of states selector

--- src/selectors/filteredStatesByChainId.ts
+++ src/selectors/filteredStatesByChainId.ts
@@ -7,8 +7,30 @@ const filteredStatesByChainId = selectorFamily({
     const simulation = get(cwSimulateEnvState);
     if (!(chainId in simulation.chains)) return {};
     
-    // TODO: construct states from contract instances
-    return {};
+    const result: Record<string, any> = {};
+    
+    const decoder = new TextDecoder();
+    const chain = simulation.chains[chainId];
+    for (const address in chain.contracts) {
+      const contract = chain.contracts[address];
+      for (const bkey of contract.storage.keys()) {
+        const key = decoder.decode(bkey);
+        const value = decoder.decode(contract.storage.get(bkey) ?? new Uint8Array(0));
+        try {
+          const json = JSON.parse(value);
+          // TODO: recursive
+          for (const prop in json) {
+            const subkey = prop.match(/['".\s-]/) ? `${key}["${prop}"]` : `${key}.${prop}`;
+            result[subkey] = json[prop] + '';
+          }
+        }
+        catch {
+          result[key] = value;
+        }
+      }
+    }
+    
+    return result;
   }
 });
 
