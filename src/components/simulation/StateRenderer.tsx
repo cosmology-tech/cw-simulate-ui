348dda4b5bb5116bcd68e3ec915509c3e8a52083
Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>
2022-10-03 17:13:17 +0530

    Fixed simulation screen bugs. More in description.

--- src/components/simulation/StateRenderer.tsx
+++ src/components/simulation/StateRenderer.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { BeforeAfterState } from "./BeforeAfterState";
-import StateMemoryTab from "./StateMemoryTab";
+import StateResponseTab from "./StateResponseTab";
 import { OutputCard } from "./OutputCard";
 import { Grid } from "@mui/material";
 import { OutputRenderer } from "./OutputRenderer";
 import { responseState } from "../../atoms/responseState";
 import { blockState } from "../../atoms/blockState";
 import { useAtomValue } from "jotai";
+import { stateResponseTabState } from "../../atoms/stateResponseTabState";
+import { useAtom } from "jotai";
 
 interface IProps {
   isFileUploaded: boolean;
-  currentState: number;
 }
 
-export const StateRenderer = ({isFileUploaded, currentState}: IProps) => {
-  const [currentTab, setCurrentTab] = React.useState("state");
+export const StateRenderer = ({ isFileUploaded }: IProps) => {
+  const [currentTab, setCurrentTab] = useAtom(stateResponseTabState);
   const [isChecked, setIsChecked] = React.useState(false);
   const response = useAtomValue(responseState);
   const currentJSON = useAtomValue(blockState);
   // TODO: Check current active state and executionHistory length.
   const isStateTraversed = false;
   return (
-    <Grid item xs={12} sx={{height: "100%"}}>
+    <Grid item xs={12} sx={{ height: "100%" }}>
       <Grid item xs={12}>
-        <StateMemoryTab
+        <StateResponseTab
           currentTab={currentTab}
           setCurrentTab={setCurrentTab}
           isChecked={isChecked}
@@ -31,13 +32,10 @@ export const StateRenderer = ({isFileUploaded, currentState}: IProps) => {
           isStateTraversed={isStateTraversed}
         />
       </Grid>
-      <Grid item xs={12} sx={{height: "80%"}}>
+      <Grid item xs={12} sx={{ height: "80%" }}>
         {currentTab === "state" ? (
           isStateTraversed ? (
-            <BeforeAfterState
-              currentState={currentState + 1}
-              isChecked={isChecked}
-            />
+            <BeforeAfterState isChecked={isChecked} />
           ) : (
             <OutputCard
               response={currentJSON}
@@ -45,7 +43,7 @@ export const StateRenderer = ({isFileUploaded, currentState}: IProps) => {
             />
           )
         ) : (
-          <OutputRenderer response={response}/>
+          <OutputRenderer response={response} />
         )}
       </Grid>
     </Grid>
---
+++ src/components/simulation/StateResponseTab.tsx
@@ -0,0 +1,52 @@
+import { Tab, Tabs } from "@mui/material";
+import React from "react";
+import DiffSwitch from "./DiffSwitch";
+
+interface IProps {
+  currentTab: string;
+  setCurrentTab: (val: string) => void;
+  setIsChecked: (val: boolean) => void;
+  isChecked: boolean;
+  isStateTraversed: boolean;
+}
+
+const StateResponseTab = ({
+  currentTab,
+  setCurrentTab,
+  isChecked,
+  setIsChecked,
+  isStateTraversed,
+}: IProps) => {
+  const onChangeHandler = (event: React.SyntheticEvent, newValue: string) => {
+    setCurrentTab(newValue);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <Tabs
+          value={currentTab}
+          onChange={onChangeHandler}
+          aria-label="State/Response"
+        >
+          <Tab value="state" label="State" />
+          <Tab value="response" label="Response" />
+          {/* <Tab value="memory" label="Memory" /> */}
+        </Tabs>
+      </div>
+      {isStateTraversed && (
+        <div>
+          <DiffSwitch isChecked={isChecked} setIsChecked={setIsChecked} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StateResponseTab;
