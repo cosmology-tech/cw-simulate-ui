bf1799f2e6c074bc3c3acdbccaf7f290598c70a9
Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>
2022-11-02 22:25:39 +0530

    New Layout (WIP)

--- src/components/simulation/Simulation.tsx
+++ src/components/simulation/Simulation.tsx
@@ -1,18 +1,137 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { useParams } from "react-router-dom";
 import SendMessage from "./SendMessage";
 import History from "./History";
-import SplitView from "./SplitView";
+// import SplitView from "./SplitView";
+import { Theme } from "@emotion/react";
+import { useAtom, useAtomValue } from "jotai";
+import { Grid, Paper, styled, SxProps } from "@mui/material";
+import { GREY_6 } from "../../configs/variables";
+import { GridSizeProps } from "../../utils/typeUtils";
+import { ExecuteQuery } from "./ExecuteQuery";
+import { StateRenderer } from "./StateRenderer";
+import StateStepper from "./StateStepper";
+import { fileUploadedState } from "../../atoms/fileUploadedState";
+import { responseState } from "../../atoms/reponseState";
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+}));
 
 const Simulation = () => {
   const { chainId, instanceAddress: contractAddress } = useParams();
-
+  const [, setResponse] = useAtom(responseState);
+  const isFileUploaded = useAtomValue(fileUploadedState);
   return (
     <SplitView className="T1Simulation-root">
-      <SendMessage contractAddress={contractAddress!} />
-      <History chainId={chainId!} contractAddress={contractAddress!} />
+      {/* <SendMessage contractAddress={contractAddress!} /> */}
+      {/* <History chainId={chainId!} contractAddress={contractAddress!} /> */}
+      <Column xs={4} className="T1Simulation-left">
+        <Widget sx={{ p: 1 }}>
+          <StateStepper chainId={chainId!} contractAddress={contractAddress!} />
+        </Widget>
+      </Column>
+      <Column xs={8} className="T1Simulation-right">
+        <Widget size={6}>
+          <ExecuteQuery
+            setResponse={setResponse}
+            contractAddress={contractAddress!}
+          />
+        </Widget>
+        <Widget size={6}>
+          <StateRenderer isFileUploaded={isFileUploaded} />
+        </Widget>
+      </Column>
     </SplitView>
   );
 };
 
 export default Simulation;
+
+interface ISplitViewProps {
+  children?: ReactNode;
+  className?: string;
+}
+function SplitView({ children, ...props }: ISplitViewProps) {
+  return (
+    <Grid
+      container
+      direction="row"
+      spacing={2}
+      sx={{
+        height: "100%",
+      }}
+      {...props}
+    >
+      {children}
+    </Grid>
+  );
+}
+
+interface IColumnProps extends GridSizeProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+function Column({ children, ...props }: IColumnProps) {
+  return (
+    <Grid
+      item
+      {...props}
+      sx={{
+        height: "100%",
+      }}
+    >
+      <Grid
+        container
+        direction="column"
+        component={StyledPaper}
+        sx={{
+          height: "100%",
+          overflow: "auto",
+          "> .T1Widget-root:not(:first-of-type)": {
+            borderTop: `1px solid ${GREY_6}`,
+          },
+        }}
+      >
+        {children}
+      </Grid>
+    </Grid>
+  );
+}
+
+interface IWidgetProps {
+  children?: ReactNode;
+  /** An optional size between 1-12. At 12, size is 100%. Lower values are multiples of 100%/12. Default is 12. */
+  size?: number;
+  className?: string;
+  sx?: SxProps<Theme>;
+}
+
+function Widget({
+  children,
+  size = 12,
+  className,
+  sx,
+  ...props
+}: IWidgetProps) {
+  return (
+    <Grid
+      item
+      sx={[
+        {
+          p: 2,
+          height: `${(100 * size) / 12}%`,
+          overflow: "auto",
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+      className={`T1Widget-root ${className}`}
+      {...props}
+    >
+      {children}
+    </Grid>
+  );
+}
