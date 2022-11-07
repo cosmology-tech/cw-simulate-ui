55c89a690de64b80377cc5468de67b0413244b9f
Bun <bunchhieng@gmail.com>
2022-11-07 16:56:14 -0500

    Update text for form

--- src/components/drawer/ConfigSubMenu.tsx
+++ src/components/drawer/ConfigSubMenu.tsx
@@ -8,28 +8,26 @@ export interface IConfigSubMenuProps {
 
 export default function ConfigSubMenu(props: IConfigSubMenuProps) {
   const {app} = useAtomValue(cwSimulateAppState);
+  const handleSaveConfig = () => {
+    //TODO: save config
+  }
+
   return (
     <>
       <SubMenuHeader title="Chain Configuration"/>
-      {/*<Typography>*/}
-      {/*  Chain Id: {app.chainId}*/}
-      {/*</Typography>*/}
-      {/*<Typography>*/}
-      {/*  Bech32Prefix: {app.bech32Prefix}*/}
-      {/*</Typography>*/}
       <Box sx={{left: 8, top: 8, position: 'relative'}}>
         <FormControl>
           <FormGroup>
             <Input id="chainId" aria-describedby="chainIdHelperText" defaultValue={app.chainId}
                    inputProps={{style: {textAlign: 'center'}}}/>
-            <FormHelperText id="chainIdHelperText">Chain Id.</FormHelperText>
+            <FormHelperText id="chainIdHelperText">Chain Id</FormHelperText>
 
             <Input id="bech32Prefix" aria-describedby="bech32PrefixHelperText"
                    defaultValue={app.bech32Prefix}
                    inputProps={{style: {textAlign: 'center'}}}/>
-            <FormHelperText id="bech32PrefixHelperText">Bech32 Prefix.</FormHelperText>
+            <FormHelperText id="bech32PrefixHelperText">Bech32 Prefix</FormHelperText>
 
-            <Button>Save</Button>
+            <Button onClick={() => handleSaveConfig}>Save</Button>
           </FormGroup>
         </FormControl>
       </Box>
