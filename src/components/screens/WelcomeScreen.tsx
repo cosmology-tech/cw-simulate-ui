787345488afa098464424d71ef80a5b4f91a8d5d
Bun <bunchhieng@gmail.com>
2022-11-07 17:05:29 -0500

    Remove Config component

--- src/components/screens/WelcomeScreen.tsx
+++ src/components/screens/WelcomeScreen.tsx
@@ -57,7 +57,7 @@ export default function WelcomeScreen() {
   useEffect(() => {
     if (file) {
       onCreateNewEnvironment().then(r => {
-        navigate("/config");
+        navigate("/accounts");
       });
     }
   }, [file]);
