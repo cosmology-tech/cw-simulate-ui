2c090db280415845ed14c37633d0cd7b65ab48b1
Kiruse <kiruse@kiruse.dev>
2022-11-21 17:56:37 +0100

    remove unused generic parameter & unused import

--- src/components/simulation/StateStepper.tsx
+++ src/components/simulation/StateStepper.tsx
@@ -8,7 +8,7 @@ import { SvgIconProps } from "@mui/material/SvgIcon";
 import { TraceLog } from "@terran-one/cw-simulate/dist/types";
 import { useAtom, useSetAtom } from "jotai";
 import { Map } from "immutable";
-import { SyntheticEvent, useEffect, useMemo, useState } from "react";
+import { SyntheticEvent, useEffect, useMemo } from "react";
 import {
   activeStepState,
   blockState,
@@ -107,7 +107,7 @@ type NumberIconProps = SvgIconProps & {
   error: string | undefined;
 };
 
-function NumberIcon<SvgIconComponent>({ number, error }: NumberIconProps) {
+function NumberIcon({ number, error }: NumberIconProps) {
   const theme = useMuiTheme();
   return (
     <Box
