26bed99b0a10d9b11cfcb5f065ce9359aaf103eb
Bun <bunchhieng@gmail.com>
2022-11-28 21:04:54 -0500

    Change R2 URL

--- functions/r2/[prefix]/[fileName].ts
+++ functions/r2/[prefix]/[fileName].ts
@@ -5,12 +5,13 @@ interface Env {
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   // Validate bucket name and prefix
   const prefix = context.params.prefix;
+  const fileName = context.params.fileName;
   if (!prefix) {
     return new Response("Invalid path", {status: 400});
   }
 
-  console.log(`Query R2 bucket for prefix: ${prefix}`);
-  const obj = await context.env.EXAMPLES_BUCKET.get(prefix);
+  console.log(`Query R2 bucket for prefix: ${prefix}/${fileName}`);
+  const obj = await context.env.EXAMPLES_BUCKET.get(`${prefix}/${fileName}`);
   if (obj === null) {
     return new Response('Not found', {status: 404});
   }
