a800762c8484341559e79e8eee57c758acd585a7
Kiruse <26940742+Kiruse@users.noreply.github.com>
2022-12-06 22:33:11 +0100

    Custom T1Accordion component for consistency (#220)
    new T1Accordion component for consistency
    
    Co-authored-by: Kiruse <kiruse@kiruse.dev>

--- src/components/simulation/tabs/LogsTab.tsx
+++ src/components/simulation/tabs/LogsTab.tsx
@@ -1,18 +1,16 @@
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { useTheme } from "@mui/material";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
+import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { TraceLog } from "@terran-one/cw-simulate";
+import { DebugLog, TraceLog } from "@terran-one/cw-simulate";
+import React, { ReactNode } from "react";
+import { T1Accordion, T1AccordionSection } from "../../T1Accordion";
 import T1JsonTree from "../../T1JsonTree";
 import { EmptyTab, IInspectorTabProps } from "./Common";
 
 export default function LogsTab({ traceLog }: IInspectorTabProps) {
   if (!traceLog) return <EmptyTab />;
 
-  let combinedLogs = combineLogs(traceLog).filter((log) => log.type === "call");
+  let combinedLogs = extractLogs(traceLog).filter((log) => log.type === "call");
 
   return (
     <Grid sx={{ height: "100%", width: "100%" }}>
@@ -26,83 +24,90 @@ export default function LogsTab({ traceLog }: IInspectorTabProps) {
             justifyContent: "center",
           }}
         >
-          <Typography variant="caption">No Logs to show.</Typography>
+          <EmptyTab>No Logs to show.</EmptyTab>
         </Grid>
       ) : (
-        combinedLogs.map((log, index) => (
-          <CallListItem
-            ix={index}
-            key={`a-${index}`}
-            call={log}
-            isLastItem={index === combinedLogs.length - 1}
-          />
-        ))
+        <T1Accordion sx={{ width: '100%' }}>
+          {combinedLogs.map((log, index) => (
+            <T1AccordionSection
+              title={<LogTitle log={log} index={index} />}
+              sx={{ width: '100%' }}
+            >
+              <LogBody log={log} />
+            </T1AccordionSection>
+          ))}
+        </T1Accordion>
       )}
     </Grid>
   );
 }
 
-const CallListItem = ({
-  call,
-  ix,
-  isLastItem,
-}: {
-  call: { args: { [k: string]: any }; result: any; fn: string };
-  ix: number;
-  isLastItem: boolean;
-}) => {
-  const theme = useTheme();
+interface ILogTitle {
+  log: DebugLog;
+  index: number;
+}
+
+const LogTitle = React.memo(({ log, index }: ILogTitle) => {
   return (
-    <Accordion
-      sx={{
-        "&.Mui-expanded": {
-          margin: "0px",
-          borderBottom: !isLastItem ? "1px solid #bccabc" : "",
-        },
-      }}
+    <Typography
+      fontFamily="JetBrains Mono"
+      sx={{ wordWrap: 'break-word' }}
     >
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-      >
-        <Typography
-          fontFamily={"JetBrains Mono"}
-          sx={{ wordWrap: "break-word" }}
-        >
-          [{ix}] {call.fn}
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <table style={{ wordBreak: "break-word" }}>
-          <T1JsonTree data={call.args} />
-        </table>
-        <Typography
-          variant="subtitle2"
-          sx={{ fontWeight: "bold", mt: 1, mb: 1 }}
-        >
-          Result
-        </Typography>
-        {call.result ? (
-          <Typography variant="body2" sx={{ wordBreak: "break-word" }}>
-            {call.result}
-          </Typography>
-        ) : (
-          <Typography variant="body2" color={theme.palette.grey[600]}>
-            None
-          </Typography>
-        )}
-      </AccordionDetails>
-    </Accordion>
-  );
+      {log.type === 'print' ? (
+        <>[{index}] Print</>
+      ) : (
+        <>[{index}] {log.fn}</>
+      )}
+    </Typography>
+  )
+});
+
+interface ILogBodyProps {
+  log: DebugLog;
+}
+
+const LogBody = ({ log }: ILogBodyProps) => {
+  const SubsectionTitle = ({ children }: { children: string }) =>
+    <Typography fontStyle="italic" fontWeight="bold">{children}</Typography>
+  
+  switch (log.type) {
+    case 'print': return (
+      <Typography fontFamily="JetBrains Mono">{log.message}</Typography>
+    )
+    case 'call': return (
+      <>
+        <SubsectionTitle>Arguments:</SubsectionTitle>
+        <Box sx={{ wordBreak: 'break-word' }}>
+          <T1JsonTree data={log.args} />
+        </Box>
+        {'result' in log && (<>
+          <SubsectionTitle>Result:</SubsectionTitle>
+          {formatResult(log.result)}
+        </>)}
+      </>
+    )
+  }
 };
 
-const combineLogs = (traceLog: TraceLog): any[] => {
+const extractLogs = (traceLog: TraceLog): DebugLog[] => {
   let res = [...traceLog.logs];
   if (traceLog.trace) {
     traceLog.trace.forEach((t) => {
-      res = res.concat(combineLogs(t));
+      res = res.concat(extractLogs(t));
     });
   }
   return res;
 };
+
+function formatResult(result: string | number): ReactNode {
+  if (typeof result !== 'string') return result.toString();
+  try {
+    const parsed = JSON.parse(result);
+    if (typeof parsed !== 'object')
+      return <Typography fontFamily="JetBrains Mono">{parsed}</Typography>
+    return <T1JsonTree data={parsed} />
+  }
+  catch {
+    return result;
+  }
+}
